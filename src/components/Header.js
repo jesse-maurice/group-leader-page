414ab7b0731de3b4809ccf96d7f139b897eae28d
Jesse-Maurice Iyoha <iyohajessy@gmail.com>
2023-07-05 16:01:52 +0100

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import image from '../images/Subtract.png';
+
+const Header = () => {
+  return (
+    <>
+      <div className="flex flex-row items-center justify-between w-full ">
+        <a className="cursor-pointer " href="/">
+          <h1 className="text-[33px] font-extrabold text-[#faf5e9] max-sm:text-3xl max-md:text-4xl">
+            WE
+            <img
+              className="inline-block mb-[6px] ml-[2px] "
+              src={image}
+              alt=""
+            ></img>
+          </h1>
+        </a>
+        <div>
+          <input
+            className=" p-3 w-[150px] h-7 bg-[#faf5e9] opacity-25 rounded-full"
+            type="text"
+            name="search"
+            value="search"
+          ></input>
+          <i class="fa-sharp fa-solid fa-magnifying-glass relative right-[25px] top-[1px]"></i>
+
+          <button className=" bg-transparent text-[#009b4d] font-semibold text-md" type="">
+            EN -
+          </button>
+          <button className=" bg-transparent ml-2 text-[#faf5e9] font-semibold text-md" type="">
+            FR
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Header
