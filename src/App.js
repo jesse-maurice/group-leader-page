539a37571f533e33f0774a59460617a3a287c2a0
Jesse-Maurice Iyoha <iyohajessy@gmail.com>
2023-11-19 00:17:21 +0100

    responsiveness done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 
 import React, { useState } from 'react';
 
-import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
 import background from './images/medium-shot-man-delivering-groceries 1.jpg';
@@ -11,18 +10,17 @@ function App() {
   const [lang, setLang] = useState("en");
   return (
     <>
-      <div className="w-full h-screen overflow-hidden font-['Raleway']">
+      <div className="w-full h-screen  font-['Raleway']">
         <div className="relative w-full h-full">
           <img
-            className="object-cover w-full h-screen transition-all"
+            className="object-cover w-full h-full fixed top-0 left-0 z-0"
             src={background}
             alt=""
           ></img>
         </div>
-        <div className="absolute inset-0 overflow-hidden">
+        <div className="absolute inset-0 z-10">
           <Header lang={lang} setLang={setLang} />
           <Main lang={lang} setLang={setLang} />
-          <Footer lang={lang} setLang={setLang} />
         </div>
       </div>
     </>
--- src/components/Header.js
+++ src/components/Header.js
@@ -5,12 +5,12 @@ import image from '../images/Subtract.png';
 const Header = ({lang, setLang}) => {
   return (
     <>
-      <div className="flex flex-row px-[140px] pt-[20px] absolute  items-center justify-between w-full ">
+      <div className="flex flex-row lg:px-[140px] max-sm:px-5 md:px-10 pt-[20px] absolute  items-center justify-between w-full ">
         <a className="cursor-pointer " href="/">
-          <h1 className="text-[25px] font-extrabold text-[#faf5e9] max-sm:text-3xl max-md:text-4xl">
+          <h1 className="text-[23px] font-extrabold text-[#faf5e9] max-sm:text-3xl max-md:text-4xl">
             WE
             <img
-              className="inline-block w-[70px] mb-[4px] ml-[2px] "
+              className="inline-block w-[60px] mb-[4px] ml-[2px] "
               src={image}
               alt=""
             ></img>
@@ -18,12 +18,12 @@ const Header = ({lang, setLang}) => {
         </a>
         <div>
           <input
-            className=" p-3 w-[200px] h-7 bg-[#faf5e9] opacity-25 rounded-full"
+            className=" p-3 w-[200px] h-7 max-sm:hidden bg-[#faf5e9] opacity-25 rounded-full"
             type="text"
             name="search"
             value={lang === "fn" ? "rechercher" : "search"}
           ></input>
-          <i class="fa-sharp fa-solid fa-magnifying-glass relative right-[25px] top-[1px]"></i>
+          <i class="fa-sharp fa-solid fa-magnifying-glass max-sm:hidden relative right-[25px] top-[1px]"></i>
 
           <button
             className=" bg-transparent text-[#faf5e9] focus:text-[#009b4d] focus:text-opacity-60 font-semibold text-md"
